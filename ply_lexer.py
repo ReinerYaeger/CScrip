f9d0984babef297d9a8214a70bf0e4248f41730c
ReinerYaeger <chevking101@gmail.com>
2024-02-26 21:29:13 -0500

    ADDING TOKENS

--- ply_lexer.py
+++ ply_lexer.py
@@ -4,8 +4,13 @@ from ply.yacc import yacc
 # --- Tokenizer
 
 # All tokens must be named in advance.
-tokens = ( 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'LPAREN', 'RPAREN',
-           'NAME', 'NUMBER' )
+tokens = ('IDENTIFIER', 'EQUAL_OP', 'ADD_OP', 'SUB_OP', 'MUL_OP', 'DIV_OP',
+          'LEFT_PAR_OP', 'RIGHT_PAR_OP', 'IF_STATEMENT', 'ELSE_STATEMENT',
+          'WHILE_STATEMENT', 'FOR_STATEMENT', 'LEFT_CURL_OP', 'RIGHT_CURL_OP',
+          'TERM_STATEMENT', 'RIGHT_PAR_OP', 'IF_STATEMENT', 'ELSE_STATEMENT', 'ELSE_IF_STATEMENT,'
+          'COLON_STATEMENT', 'SEMI_COLON_STATEMENT', 'SINGLE_QUOTES', 'DOUBLE_QUOTES',
+          'int_declaration', 'float_declaration', 'single_quotes', 'double_quotes',
+          'COLON_STATEMENT', 'SEMI_COLON_STATEMENT', 'SINGLE_QUOTES', 'DOUBLE_QUOTES',)
 
 # Ignored characters
 t_ignore = ' \t'
@@ -19,6 +24,7 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
+
 # A function can be used if there is an associated action.
 # Write the matching regex in the docstring.
 def t_NUMBER(t):
@@ -26,18 +32,22 @@ def t_NUMBER(t):
     t.value = int(t.value)
     return t
 
+
 # Ignored token with an action associated with it
 def t_ignore_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count('\n')
+
 
 # Error handler for illegal characters
 def t_error(t):
     print(f'Illegal character {t.value[0]!r}')
     t.lexer.skip(1)
 
+
 # Build the lexer object
 lexer = lex()
+
 
 # --- Parser
 
@@ -55,11 +65,13 @@ def p_expression(p):
     #
     p[0] = ('binop', p[2], p[1], p[3])
 
+
 def p_expression_term(p):
     '''
     expression : term
     '''
     p[0] = p[1]
+
 
 def p_term(p):
     '''
@@ -68,11 +80,13 @@ def p_term(p):
     '''
     p[0] = ('binop', p[2], p[1], p[3])
 
+
 def p_term_factor(p):
     '''
     term : factor
     '''
     p[0] = p[1]
+
 
 def p_factor_number(p):
     '''
@@ -80,11 +94,13 @@ def p_factor_number(p):
     '''
     p[0] = ('number', p[1])
 
+
 def p_factor_name(p):
     '''
     factor : NAME
     '''
     p[0] = ('name', p[1])
+
 
 def p_factor_unary(p):
     '''
@@ -93,14 +109,17 @@ def p_factor_unary(p):
     '''
     p[0] = ('unary', p[1], p[2])
 
+
 def p_factor_grouped(p):
     '''
     factor : LPAREN expression RPAREN
     '''
     p[0] = ('grouped', p[2])
 
+
 def p_error(p):
     print(f'Syntax error at {p.value!r}')
+
 
 # Build the parser
 parser = yacc()
@@ -108,4 +127,3 @@ parser = yacc()
 # Parse an expression
 ast = parser.parse('2 * 3 + 4 * (5 - x)')
 print(ast)
-
