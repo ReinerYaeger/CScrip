8fafaa30586dfd1ecf6d887739a386ae57d756ef
Matthewsamuels-22 <113641022+Matthewsamuels-22@users.noreply.github.com>
2024-03-05 22:01:09 -0500

    updated tokens

--- ply_lexer.py
+++ ply_lexer.py
@@ -1,19 +1,19 @@
 import ply.lex as lex
+import re
 
 # --- Tokenizer
 
 # All tokens must be named in advance.
 tokens = (
-    'IDENTIFIER', 'EQUAL_OP', 'EQUIVALENT_OP', 'ADD_OP', 'POSITIVE_OP', 'NEGATIVE_OP', 'SUB_OP', 'MUL_OP', 'DIV_OP',
-    'LEFT_PAR_OP', 'RIGHT_PAR_OP', 'IF_STATEMENT', 'ELSE_STATEMENT', 'WHILE_STATEMENT', 'FOR_STATEMENT', 'LEFT_CURL_OP',
-    'RIGHT_CURL_OP', 'FULL_STOP_STATEMENT', 'IF_STATEMENT', 'ELSE_STATEMENT', 'COLON_STATEMENT', 'SEMI_COLON_STATEMENT',
+    'IF_STATEMENT', 'ELSE_STATEMENT', 'EQUAL_OP', 'EQUIVALENT_OP', 'ADD_OP', 'POSITIVE_OP', 'NEGATIVE_OP', 'SUB_OP',
+    'MUL_OP', 'DIV_OP', 'LEFT_PAR_OP', 'RIGHT_PAR_OP', 'WHILE_STATEMENT', 'FOR_STATEMENT', 'LEFT_CURL_OP',
+    'RIGHT_CURL_OP', 'FULL_STOP_STATEMENT', 'COLON_STATEMENT', 'SEMI_COLON_STATEMENT',
     'INT_DECLARATION', 'FLOAT_DECLARATION', 'DOUBLE_DECLARATION', 'STRING_DECLARATION', 'CHARACTER_DECLARATION',
-    'BOOL_STATEMENT', 'VOID_STATEMENT', 'CLASS_STATEMENT', 'OBJECT_STATEMENT', 'STRUCT_STATEMENT', 'DO_STATEMENT')
+    'BOOL_STATEMENT', 'VOID_STATEMENT', 'CLASS_STATEMENT', 'OBJECT_STATEMENT', 'STRUCT_STATEMENT', 'DO_STATEMENT',
+    'ELSEIF_STATEMENT', 'SINGLE_QUOTES', 'DOUBLE_QUOTES', 'INT_LITERAL', 'IDENTIFIER')
 
 # Ignored characters
-t_ignore = ' \t'
-
-t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_ignore = ' \t\n'
 t_EQUAL_OP = r'='
 t_EQUIVALENT_OP = r'=='
 t_ADD_OP = r'\+\+'
@@ -27,20 +27,14 @@ t_RIGHT_PAR_OP = r'\)'
 t_RIGHT_CURL_OP = r'\{'
 t_LEFT_CURL_OP = r'\}'
 t_FULL_STOP_STATEMENT = r'\.'
-t_COLON_STATEMENT = r':'
+t_COLON_STATEMENT = r'\:'
 t_SEMI_COLON_STATEMENT = r';'
-t_INT_DECLARATION = r'int'
-t_FLOAT_DECLARATION = r'float'
-t_DOUBLE_DECLARATION = r'double'
-t_STRING_DECLARATION = r'string'
-t_CHARACTER_DECLARATION = r'char'
 t_BOOL_STATEMENT = r'bool'
 t_VOID_STATEMENT = r'void'
 t_CLASS_STATEMENT = r'class'
 t_OBJECT_STATEMENT = r'object'
 t_STRUCT_STATEMENT = r'struct'
 t_DO_STATEMENT = r'do'
-t_IF_STATEMENT = r'if'
 t_ELSE_STATEMENT = r'else'
 t_WHILE_STATEMENT = r'while'
 t_FOR_STATEMENT = r'for'
@@ -48,6 +42,13 @@ t_SINGLE_QUOTES = r'\''
 t_DOUBLE_QUOTES = r'\"'
 t_INT_LITERAL = r'\d+'
 t_ELSEIF_STATEMENT = r'elseif'
+t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
+print()
+
+reserved = {
+    'if': 'IF',
+    'do': 'DO'
+}
 
 
 # A function can be used if there is an associated action.
@@ -74,86 +75,29 @@ def t_STRING_DECLARATION(t):
     return t
 
 
-# Define a rule for characters
 def t_CHARACTER_DECLARATION(t):
     """Recognize characters enclosed in single quotes."""
     r"'.'"
-    t.value = t.value[1]  # Remove quotes
-    return t
-
-
-def t_BOOL_STATEMENT(t):
-    """Recognize characters enclosed in single quotes."""
-    r'True|False'
-    t.value = t.value == 'True'
-    return t
-
-
-def t_CLASS_STATEMENT(t):
-    """Recognize statement that start with class."""
-    r'class'
-    return t
-
-
-def t_VOID_STATEMENT(t):
-    """Recognize statement that has void in it."""
-    r'void'
+    t.value = t.value[1]  # Extract the character from within the quotes
     return t
 
 
 def t_IF_STATEMENT(t):
-    """Recognize if statements."""
-    r'(IF|if)'
+    """Recognize the 'if' keyword as an IF_STATEMENT."""
+    r'[if]'
+    if t.value.lower() == 'if':
+        t.type = 'IF_STATEMENT'
     return t
 
 
-def t_FULL_STATEMENT(t):
-    """Recognize Full stop statements."""
-    r'.'
-    return t
-
-
-def t_ELSE_STATEMENT(t):
-    """Recognize else statements."""
-    r'(ELSE|else)'
-    return t
-
-
-def t_WHILE_STATEMENT(t):
-    """Recognize while statements."""
-    r'(WHILE|while)'
-    return t
-
-
-def t_DO_STATEMENT(t):
-    """Recognize Do statements."""
-    r'(DO|do)'
-    return t
-
-
-def t_FOR_STATEMENT(t):
-    """Recognize For statements."""
-    r'(FOR|for)'
-    return t
-
-
-def t_STRUCT_STATEMENT(t):
-    """Recognize Struct statements."""
-    r'Struct'
-    return t
-
-
-def t_OBJECT_STATEMENT(t):
-    """Recognize Object statements."""
-    r'Object'
-    return t
+# Define other tokens...
 
 
 # Ignored token with an action associated with it
-def t_ignore_newline(t):
+def t_newline(t):
     """Recognize newline numbers."""
     r'\n+'
-    t.lexer.lineno += t.value.count('\n')
+    t.lexer.lineno += len(t.value)
 
 
 # Error handler for illegal characters
@@ -166,7 +110,6 @@ def t_error(t):
 
 # Build the lexer
 lexer = lex.lex()
-
 # Now you can use the lexer object to tokenize input
 with open('test.lang', 'r') as file:
     content = file.read()
