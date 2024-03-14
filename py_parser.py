31c41b2481a28e3918b30e8a46bf003a118e8340
ReinerYaeger <chevking101@gmail.com>
2024-03-13 20:40:54 -0500

    somewhat working parser

--- py_parser.py
+++ py_parser.py
@@ -3,15 +3,18 @@ import ply.yacc as yacc
 # Tokens from the lexer
 from lex import tokens, lexer
 
+
 def p_program_start(p):
     ''' program_start : program
                       | arithmetic_statement
                       '''
     p[0] = p[1]
 
+
 def p_empty(p):
     ''' empty : '''
     p[0] = None
+
 
 def p_program(p):
     '''program : type_declaration main_statement left_curl_op statement right_curl_op
@@ -22,6 +25,7 @@ def p_program(p):
     else:
         p[0] = None
 
+
 def p_type_declaration(p):
     '''type_declaration : int_declaration
                         | float_declaration
@@ -29,6 +33,7 @@ def p_type_declaration(p):
                         | string_declaration
                         | char_declaration'''
     p[0] = p[1]
+
 
 def p_statement(p):
     '''statement : function_call_statement
@@ -41,18 +46,22 @@ def p_statement(p):
                  '''
     p[0] = p[1]
 
+
 def p_conditional_statement(p):
     '''conditional_statement : empty'''
     p[0] = p[1]
+
 
 def p_inequalities(p):
     '''inequalities :  inequalities_sym identifier
                     | literal_or_identifier inequalities_sym  literal_or_identifier'''
 
+
 def p_literal_or_identifier(p):
     '''literal_or_identifier : identifier
                              | data_literal'''
     p[0] = p[1]
+
 
 def p_inequalities_sym(p):
     '''inequalities_sym : equivalent_op
@@ -63,18 +72,23 @@ def p_inequalities_sym(p):
                         | '''
     p[0] = p[1]
 
+
 def p_function_call_statement(p):
     '''function_call_statement : empty'''
     p[0] = p[1]
 
+
 def p_loop_statement(p):
     '''loop_statement : while_statement inequalities left_curl_op statement right_curl_op
-                      | for_statement  inequalities left_curl_op statement right_curl_op  '''
-    p[0] = p[1]
+                      | for_statement semi_colon_statement semi_colon_statement left_curl_op statement right_curl_op
+                      | for_statement arithmetic_statement semi_colon_statement inequalities semi_colon_statement arithmetic_statement left_curl_op statement right_curl_op
+                      | do_statement inequalities left_curl_op statement right_curl_op'''
+
 
 def p_arithmetic_statement(p):
     '''arithmetic_statement : arithmetic_expr'''
     p[0] = p[1]
+
 
 def p_arithmetic_expr(p):
     '''arithmetic_expr : literal_or_identifier assign_op arithmetic_expr
@@ -88,24 +102,31 @@ def p_arithmetic_expr(p):
     else:
         p[0] = (p[2], p[1], p[3])
 
+
 def p_arithmetic_op(p):
-    '''arithmetic_op : add_op
+    '''arithmetic_op : expo_op
+                    | mul_op
+                    | div_op
+                    | add_op
                      | sub_op
-                     | expo_op
-                     | mul_op
-                     | div_op'''
+                     '''
     p[0] = p[1]
+
 
 def p_data_literal(p):
     '''data_literal : int_literal
                     | float_literal
                     | double_literal
-                    | bool_literal'''
+                    | bool_literal
+                    | char_literal
+                    | string_literal'''
     p[0] = p[1]
+
 
 def p_error(p):
     '''error : empty'''
     print("Syntax error in input!")
+
 
 # Build the parser
 parser = yacc.yacc()
