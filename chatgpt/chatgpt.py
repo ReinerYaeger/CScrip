1d6e77e1876fb60db3183c6a964b8b7fb55507f3
Matthewsamuels-22 <113641022+Matthewsamuels-22@users.noreply.github.com>
2024-04-06 08:52:38 -0500

    added chatgpt

--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -1,22 +1,30 @@
-import os
+import os.path
 
 import openai
 from dotenv import load_dotenv
-
 from openai import OpenAI
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, load_index_from_storage, StorageContext
 
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-def cscrip_chatgpt(prompt):
-    responses = openai.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
-        messages=[{"role": "user", "content": prompt}]
-    )
-    return responses.choices[0].message.content
-
-
-def chat():
-    return None
+def custom_chat(prompt):
+    data_path = "chatgpt/data"
+    PERSIST_DIR = "chatgpt/storage"
+    if not os.path.exists(PERSIST_DIR):
+        try:
+            documents = SimpleDirectoryReader("chatgpt/data").load_data()
+        except Exception as e:
+            print(f"Error loading data from '{data_path}': {e}")
+            raise
+        index = VectorStoreIndex.from_documents(documents)
+        index.storage_context.persist(persist_dir=PERSIST_DIR)
+    else:
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        index = load_index_from_storage(storage_context)
+    query_engine = index.as_query_engine()
+    response = query_engine.query(prompt)
+    print(response)
+    return response
